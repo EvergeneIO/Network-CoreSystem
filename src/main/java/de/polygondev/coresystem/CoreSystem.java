5f72cc4dfee9a20775bb4bd7613771a23c1b2f19
ChiAkuma <chiakuma13@gmail.com>
2020-05-27 16:44:49 +0200

    Initial commit

---
+++ src/main/java/de/polygondev/coresystem/CoreSystem.java
@@ -0,0 +1,17 @@
+package de.polygondev.coresystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CoreSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
